55baa91bdb49232392fcb76745eebf3d462c304c
alxndrsn <alxndrsn>
2022-10-26 08:44:38 +0000

    wip

---
+++ get-migration-seed.js
@@ -0,0 +1,26 @@
+const log = (...args) => console.error('[get-migration-seed]', ...args);
+
+log('Loading dependencies...');
+
+const { Client } = require('pg');
+const config = require('config').get('default.database');
+
+const { password, ...redactedConfig } = config;
+log('DB config:', redactedConfig);
+
+(async () => {
+	log('Connecting to DB...');
+
+  const client = new Client(config);
+	await client.connect();
+
+	log('Connected OK; fetching version...');
+
+	const { rows } = await client.query(`SELECT value->'ok' AS is_ok FROM config WHERE key='migration-seed'`);
+	if(rows.length !== 1) throw new Error('Wrong result count:', rows);
+
+	console.log(rows[0].is_ok);
+
+	log('Complete.');
+	process.exit();
+})();
---
+++ seed-db.js
@@ -0,0 +1,23 @@
+const log = (...args) => console.error('[seed-db]', ...args);
+
+log('Loading dependencies...');
+
+const { Client } = require('pg');
+const config = require('config').get('default.database');
+
+const { password, ...redactedConfig } = config;
+log('DB config:', redactedConfig);
+
+(async () => {
+	log('Connecting to DB...');
+
+  const client = new Client(config);
+	await client.connect();
+
+	log('Connected OK; fetching version...');
+
+	await client.query(`INSERT INTO config (key, value) VALUES('migration-seed', '{"ok":true}')`);
+
+	log('Complete.');
+	process.exit();
+})();
