bc832f09f14e3174bbb195634565df0c0b08be8f
alxndrsn <alxndrsn>
2022-10-26 10:28:26 +0300

    wip

---
+++ get-postgres-version.js
@@ -0,0 +1,30 @@
+const log = (...args) => console.error('[get-postgres-version]', ...args);
+
+log('Loading dependencies...');
+
+const { Client } = require('pg');
+const config = require('config').get('default.database');
+
+const { password, ...redactedConfig } = config;
+log('DB config:', redactedConfig);
+
+(async () => {
+	log('Connecting to DB...');
+
+  const client = new Client(config);
+	await client.connect();
+
+	log('Connected OK; fetching version...');
+
+	const { rows } = await client.query('SELECT VERSION()');
+	const fullVersion = rows[0].version.match(/^PostgreSQL (\S+) /)[1];
+
+	log('Got full version:', fullVersion);
+
+	const parts = fullVersion.split('.', 2);
+
+	if(parts[0] < 10) console.log(parts.join('.'));
+	else              console.log(parts[0]);
+
+	log('Complete.');
+})();
