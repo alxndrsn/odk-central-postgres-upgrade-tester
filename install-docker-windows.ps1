d7c45778c0e3bb19300f3935dc06f1903d534c58
alxndrsn <alxndrsn>
2022-10-31 21:18:30 +0000

    windows: download with curl

--- install-docker-windows.ps1
+++ install-docker-windows.ps1
@@ -1,11 +1,7 @@
 # source: https://gist.github.com/chamindac/6045561f84f8548b052f523114583d41
 Write-Host "Installing Docker..."
 
-Write-Host "Downloading..."
-Invoke-WebRequest -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerInstaller.exe -UseBasicParsing
-Write-Host "Download complete."
-
-start-process .\DockerInstaller.exe "install --quiet" -Wait -NoNewWindow
+start-process .\installer.exe "install --quiet" -Wait -NoNewWindow
 cd "C:\Program Files\Docker\Docker\"
 Write-Host "Installing..."
 $ProgressPreference = 'SilentlyContinue'
