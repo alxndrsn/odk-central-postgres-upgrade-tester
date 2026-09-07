94812be70077af0073d9c49834ed28eb10389ac5
alxndrsn <alxndrsn>
2026-09-07 12:31:45 +0000

    retry loads

--- lib.bash
+++ lib.bash
@@ -145,7 +145,7 @@ confirm_postgres_version() {
       log "[confirm_postgres_version] Postgres version confirmed: $expectedVersion"
       return
     elif [[ "$actualVersion" = "" ]] || [[ "$actualVersion" = "ECONNREFUSED" ]]; then
-      if [[ "$retries" -lt 5 ]]; then
+      if [[ "$retries" -lt 25 ]]; then
         log "[confirm_postgres_version] Retrying..."
         (( ++retries ))
         sleep 2
