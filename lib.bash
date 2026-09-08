f96f2242661b21fa7378152c508ba1b86c6fbb0a
alxndrsn <alxndrsn>
2026-09-08 09:35:40 +0000

    ECONNREFUSED not ENOTFOUND

--- lib.bash
+++ lib.bash
@@ -162,8 +162,7 @@ confirm_postgres_version() {
       log "[confirm_postgres_version] Postgres version confirmed: $expectedVersion"
       return
     elif [[ "$actualVersion" = "" ]] || \
-         [[ "$actualVersion" = "ECONNREFUSED" ]] || \
-         [[ "$actualVersion" = "ENOTFOUND" ]]; then
+         [[ "$actualVersion" = "ECONNREFUSED" ]]; then
       if [[ "$retries" -lt 5 ]]; then
         log "[confirm_postgres_version] Retrying..."
         (( ++retries ))
