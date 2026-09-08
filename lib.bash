e45140b80506f0a206742bfc72cd1d7c6cb6ee16
alxndrsn <alxndrsn>
2026-09-08 08:17:08 +0000

    update expected version for test_restart

--- lib.bash
+++ lib.bash
@@ -281,7 +281,7 @@ test_restart() {
   restart_containers
   wait_for_service_container
   confirm_backend_running_ok
-  confirm_postgres_version 14
+  confirm_postgres_version 18
   confirm_seed_data
   log "Containers restarted ok."
 }
