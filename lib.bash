994e95b7db23c039b96c285a16e9a27c8be3f9db
alxndrsn <alxndrsn>
2026-09-08 08:03:35 +0000

    fix refs

--- lib.bash
+++ lib.bash
@@ -33,7 +33,7 @@ configure_environment() {
 
   baseRepo=https://github.com/alxndrsn/odk-central.git # TODO this will need to be updated to getodk/central
   initialBranch="${INITIAL_BRANCH-upgrade-pg-9.6}"
-  initialVesion="$(sed -E 's/upgrade-pg-([0-9.]+)(-official)?/\1/' <<<"$initialBranch")"
+  initialVersion="$(sed -E 's/upgrade-pg-([0-9.]+)(-official)?/\1/' <<<"$initialBranch")"
   targetBranch="upgrade-pg-18"
   # include a nonce in the test directory, as we will not own the postgres data
   # directory by the end of the test.  An alternative would be to `sudo` when
