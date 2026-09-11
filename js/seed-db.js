2e0b8e6929eb81f8096522522db3b90cfcd7fc13
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-09-11 10:56:42 +0300

    seed-db: handle promise rejection (#40)

--- js/seed-db.js
+++ js/seed-db.js
@@ -11,15 +11,20 @@ log('DB env vars:');
 Object.entries(process.env).filter(([ k ]) => k.startsWith('PG')).sort(([k1], [k2]) => k1<k2?-1:1).forEach(([ k, v ]) => log(`  ${k}=${v}`));
 
 (async () => {
-  log('Connecting to DB...');
+  try {
+    log('Connecting to DB...');
 
-  const client = new Client(config);
-  await client.connect();
+    const client = new Client(config);
+    await client.connect();
 
-  log('Connected OK; inserting...');
+    log('Connected OK; inserting...');
 
-  await client.query(`INSERT INTO config (key, value) VALUES('db-seed', '{"ok":true}')`);
+    await client.query(`INSERT INTO config (key, value) VALUES('db-seed', '{"ok":true}')`);
 
-  log('Complete.');
-  process.exit();
+    log('Complete.');
+    process.exit();
+  } catch(err) {
+    console.log(err);
+    process.exit(1);
+  }
 })();
