36cf8ee5e0fafe3597b91754a06894f3fc1376f3
alxndrsn <alxndrsn>
2026-09-17 09:44:44 +0000

    updates

--- lib.bash
+++ lib.bash
@@ -140,6 +140,7 @@ restart_containers() {
   log "[restart_containers] Restarting containers..."
   docker compose stop
   docker compose up --remove-orphans --detach
+./docker-volumes/print-owners
   log "[restart_containers] Containers restarted OK."
 }
 
@@ -301,6 +302,7 @@ setup_standard() {
   log "[setup_standard] Building and starting containers..."
   docker compose build
   docker compose up --remove-orphans --detach
+./docker-volumes/print-owners
 
   wait_for_service_container
 
