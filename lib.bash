586de02554bd363e07f412b5f67b46d1f0125bed
alxndrsn <alxndrsn>
2026-09-17 09:57:59 +0000

    try better

--- lib.bash
+++ lib.bash
@@ -140,7 +140,7 @@ restart_containers() {
   log "[restart_containers] Restarting containers..."
   docker compose stop
   docker compose up --remove-orphans --detach
-  ../../../../docker-volumes/print-owners
+  "$(dirname "$0")/docker-volumes/print-owners"
   log "[restart_containers] Containers restarted OK."
 }
 
@@ -302,7 +302,7 @@ setup_standard() {
   log "[setup_standard] Building and starting containers..."
   docker compose build
   docker compose up --remove-orphans --detach
-  ../../../../docker-volumes/print-owners
+  "$(dirname "$0")/docker-volumes/print-owners"
 
   wait_for_service_container
 
