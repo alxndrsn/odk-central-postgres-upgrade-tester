927001aabb1b6a45fdf5f86599221a73f86d28d1
alxndrsn <alxndrsn>
2026-09-21 09:41:47 +0000

    try to provoke failure due to subpath absence

--- lib.bash
+++ lib.bash
@@ -102,6 +102,7 @@ git_checkout() {
   log "[git_checkout] Checking out '$1'..."
   git checkout -- docker-compose.yml
   git checkout "$1"
+  sed -i -E -e '/^\s*subpath:/d' docker-compose.yml
   touch .env
   git submodule init
   git submodule update --init --jobs 16
